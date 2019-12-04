57026c6110e7c52ff7efbeecaaf8c87a696280cb
Miyakawa <haru19970409@gmail.com>
2019-12-04 09:28:41 +0900

    こんな感じ

--- motor_2/motor_2.ino
+++ motor_2/motor_2.ino
@@ -1,45 +1,67 @@
 #include <Servo.h>
 
-//kukku
 Servo myservo_1;//Servoオブジェクトの宣言
 Servo myservo_2;
-char data[10];   // 文字列格納用
-char data_2[10];   // 文字列格納用
-int i = 0;  // 文字数のカウンタ
-
+String str;
+String cmds[2] = {"\0"}; // 分割された文字列を格納する配列 
   
 void setup() {
   Serial.begin(9600);
-  //pinMode(13, OUTPUT);
-  //digitalWrite(13, LOW);  //  初期化
   myservo_1.attach(9);//servo変数をピンに割り当てる、ここでは9番ピン
   myservo_2.attach(10);
-  myservo_1.write(0);//角度を指定、ここでは90度
+  myservo_1.write(0);//角度を指定
   myservo_2.write(0);
 }
 
+
 String serialRead(){
   // データ受信したとき
-  if (Serial.available()) {
-    // 1文字ずつ読み込み
-    data[i] = Serial.read();
-    // 文字数が10以上 or 終端文字なら終了
-    if (i > 10 || data[i] == '\0') i = 0;
-    else { i++; }
-    if(data[0] == "S"){
-      for(int j=0; j++; j<10) data_2[j] == data[j+1];
-    }
+  if (Serial.available()>0) {
+    str = Serial.readStringUntil('\n');
+    return str;
   }
-  return String(data_2);
+  return "NULL";
 }
 
+int split(String data, char delimiter, String *dst){
+    int index = 0;
+    int arraySize = (sizeof(data)/sizeof((data)[0]));  
+    int datalength = data.length();
+    for (int i = 0; i < datalength; i++) {
+        char tmp = data.charAt(i);
+        if ( tmp == delimiter ) {
+            index++;
+            if ( index > (arraySize - 1)) return -1;
+        }
+        else dst[index] += tmp;
+    }
+    return (index + 1);
+}
+ 
 void loop() {
-  //if (Serial.available()){
-    String cmd = serialRead();
-    float fcmd = cmd.toFloat();
-    Serial.print(fcmd);//送信。受信データをそのまま送り返す。
-    myservo_1.write(fcmd);
-    //myservo_2.write(fcmd);
+    String cmd = serialRead();  
+    if(cmd == "NULL");
+    else{
+      //Serial.println(cmd);
+      int index = split(cmd, ',', cmds);
+      if(index == 2){
+      //Serial.println(index);
+  // 結果表示    
+        float fcmds_1 = cmds[0].toFloat();
+        float fcmds_2 = cmds[1].toFloat();
+     
+        myservo_1.write(fcmds_1);
+        myservo_2.write(fcmds_2);
+        cmds[0] = '\0'; 
+        cmds[1] = '\0';
+             
+        Serial.print("ok");//送信。
+        //Serial.println(cmds[0]);
+        //Serial.println(cmds[1]);
+      }
+      
+    }
+    
     //delay(1000); 
  // }
 }
